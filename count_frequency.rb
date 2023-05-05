c810ace4f9c28e1432304c4d003e0678b90df3d8
hstonehouse <hln.stonehouse@gmail.com>
2023-05-05 15:40:53 +1000

    Replaced count_frequency method with tally method

--- count_frequency.rb
+++ count_frequency.rb
@@ -1,3 +1,5 @@
+# This method is obsolete! It can be replaced with the 'tally' method
+
 def count_frequency(word_list)
   counts = Hash.new(0)
   word_list.each do |word|
--- sorted_word_count.rb
+++ sorted_word_count.rb
@@ -4,7 +4,8 @@ require_relative "count_frequency"
 raw_text = "The problem breaks down into two parts. First, given some text as a string, return a list of words. That sounds like an array. Then, build a count for each distinct word. That sounds like a use for a hash---we can index it with the word and use the corresponding entry to keep a count."
 
 word_list = words_from_string(raw_text)
-counts = count_frequency(word_list)
+# counts = count_frequency(word_list) <- obsolete - replace with tally method
+counts = word_list.tally
 sorted = counts.sort_by {| word, count | count }
 top_five = sorted.last(5)
 
