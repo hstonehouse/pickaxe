de9f6282a9ebff8663d4f7af3bd9646f975a48df
hstonehouse <hln.stonehouse@gmail.com>
2023-05-09 09:14:15 +1000

    Commented out obsolete code
    instead of deleting it so that there is a history

--- sorted_word_count.rb
+++ sorted_word_count.rb
@@ -1,5 +1,5 @@
 require_relative "words_from_string"
-require_relative "count_frequency"
+# require_relative "count_frequency" <- obsolete
 
 raw_text = "The problem breaks down into two parts. First, given some text as a string, return a list of words. That sounds like an array. Then, build a count for each distinct word. That sounds like a use for a hash---we can index it with the word and use the corresponding entry to keep a count."
 
@@ -8,7 +8,6 @@ word_list = words_from_string(raw_text)
 counts = word_list.tally
 sorted = counts.sort_by {| word, count | count }
 top_five = sorted.last(5)
-
 # top_five.reverse_each do |word, count|
 #   puts "#{word}: #{count}"
 # end
